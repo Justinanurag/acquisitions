93e8c5e019bace10313b525b664eaaf6fe8d49ea
justinanurag <justinanurag0.2@gmail.com>
2026-01-31 21:07:08 +0530

    Set up Neon Postgres with Drizzle ORM

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.status(200).send("Hello from Acqusitions!!")
-})
+    res.status(200).send("Hello from Acqusitions!!");
+});
 
 export default app;
